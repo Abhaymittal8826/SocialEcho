56a2b4b2dbb43cee8f6e29950a23b4013fc99959
Abhay Mittal <sirabhaymittal@gmail.com>
2025-06-30 01:01:53 +0530

    Permission to Frontend

--- chatback/src/app.js
+++ chatback/src/app.js
@@ -9,6 +9,11 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors({
+  origin: ['https://social-echo-eight.vercel.app','http://localhost:5173'],
+  credentials: true,
+}));
+
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ limit: '16kb', extended: true }));
